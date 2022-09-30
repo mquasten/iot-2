b38611ba8eac44da7b1648c3384441caa72d91b0
Manfred Quasten <manfred.quasten@binaris-informatik.de>
2022-09-30 16:23:09 +0200

    Projekte angelegt

---
+++ iot2.batch/src/test/java/de/mq/iot2/batch/support/GetThePartyStarted.java
@@ -0,0 +1,13 @@
+package de.mq.iot2.batch.support;
+
+import org.junit.jupiter.api.Test;
+
+class GetThePartyStarted {
+
+	@Test
+	void testit() {
+		System.out.println("Ich bin ein Test.");
+
+	}
+
+}
---
+++ iot2.domain/src/test/java/de/mq/iot2/domain/calendar/GetThePartyStarted.java
@@ -0,0 +1,13 @@
+package de.mq.iot2.domain.calendar;
+
+import org.junit.jupiter.api.Test;
+
+class GetThePartyStarted {
+
+	@Test
+	void testit() {
+		System.out.println("Ich bin ein Test.");
+
+	}
+
+}
