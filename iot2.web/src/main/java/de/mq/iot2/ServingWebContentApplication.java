e146a51004ea772fb3e7d9718ece53889dcfdfb9
Manfred Quasten <manfred.quasten@binaris-informatik.de>
2022-12-05 09:22:20 +0100

    setup web

---
+++ iot2.web/src/main/java/de/mq/iot2/ServingWebContentApplication.java
@@ -0,0 +1,23 @@
+package de.mq.iot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+
+@SpringBootApplication
+@EnableJpaRepositories("de.mq.iot2")
+@EntityScan(basePackages = "de.mq.iot2")
+@ComponentScan(basePackages = "de.mq.iot2")
+@EnableTransactionManagement()
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
+}
---
+++ iot2.web/src/main/java/de/mq/iot2/sysvars/support/SystemVariablesConfiguration.java
@@ -0,0 +1,20 @@
+package de.mq.iot2.sysvars.support;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@PropertySource(value="classpath:/application.properties" ,ignoreResourceNotFound=true)
+class SystemVariablesConfiguration {
+
+	@Bean
+	RestOperations restOperations() {
+		final var clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		return new RestTemplate(clientHttpRequestFactory);
+	}
+
+}
