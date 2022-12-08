df6635221682e151871ddce100c76c99d5fb7d4a
Manfred Quasten <manfred.quasten@binaris-informatik.de>
2022-12-08 08:17:35 +0100

    add backup db

--- iot2.batch/src/main/scripts/iot.bat
+++ iot2.batch/src/main/scripts/iot.bat
@@ -6,4 +6,6 @@ java -jar C:\iot/iot2Batch.jar -c end-of-day >> C:\iot/log/iot.log 2>>&1
 echo start cleanup at: %DATE% %TIME%  >> C:\iot/log/iot.log 2>>&1
 java -jar C:\iot/iot2Batch.jar -c cleanup >> C:\iot/log/iot.log 2>>&1
 
+java -cp "h2/h2-2.1.214.jar"  org.h2.tools.Script -url jdbc:h2:tcp://localhost/iot -user sa -password sa -script backup/backup.sql
+
 shutdown /s /f /t 00 >>  C:\iot/log/iot.log 2>>&1
