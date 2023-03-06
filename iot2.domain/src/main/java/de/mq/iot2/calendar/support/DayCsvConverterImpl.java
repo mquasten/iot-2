f3501c4ed7fe5c39a1d38371594c79d6237e53a1
Manfred Quasten <manfred.quasten@binaris-informatik.de>
2023-03-06 16:56:18 +0100

    calendar csv export

--- iot2.domain/src/main/java/de/mq/iot2/calendar/support/DayCsvConverterImpl.java
+++ iot2.domain/src/main/java/de/mq/iot2/calendar/support/DayCsvConverterImpl.java
@@ -1,12 +1,14 @@
 package de.mq.iot2.calendar.support;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.converter.Converter;
@@ -14,13 +16,16 @@ import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
+import de.mq.iot2.calendar.Cycle;
 import de.mq.iot2.calendar.Day;
+import de.mq.iot2.calendar.DayGroup;
 import de.mq.iot2.support.IdUtil;
 import jakarta.persistence.DiscriminatorValue;
 
 @Component
 class DayCsvConverterImpl implements Converter<Pair<Day<?>, boolean[]>, String[]> {
 
+	static final String BOOLEAN_ARRAY_WRONG_SIZE = "2 flags required.";
 	private static final String OUOTE = "\"";
 	private final String csvDelimiter;
 	private final Map<Class<?>, Function<AbstractDay<?>, String>> valueConverter = new HashMap<>();
@@ -28,42 +33,75 @@ class DayCsvConverterImpl implements Converter<Pair<Day<?>, boolean[]>, String[]
 	DayCsvConverterImpl(@Value("${iot2.csv.delimiter:;}") final String csvDelimiter) {
 		this.csvDelimiter = csvDelimiter;
 		valueConverter.put(GaussDayImpl.class, this::gaussDay);
+		valueConverter.put(DayOfMonthImpl.class, this::dayOfMonth);
+		valueConverter.put(DayOfWeekDayImpl.class, this::dayOfWeek);
+		valueConverter.put(LocalDateDayImp.class, this::localDateDay);
 	}
 
 	private String gaussDay(final AbstractDay<?> day) {
 		return "" + day.signum() * day.split(3)[0];
 	}
 
+	private String dayOfMonth(final AbstractDay<?> day) {
+		final int[] values = day.split(2);
+		return numberValue(values[1], 2) + "." + numberValue(values[0], 2);
+	}
+
+	private String dayOfWeek(final AbstractDay<?> day) {
+		return "" + day.split(1)[0];
+	}
+
+	private String localDateDay(final AbstractDay<?> day) {
+		final int[] values = day.split(4, 2);
+		return values[2] + "." + numberValue(values[1], 2) + "." + numberValue(values[0], 2);
+	}
+
 	@Override
 	public String[] convert(Pair<Day<?>, boolean[]> pair) {
 
 		Assert.notNull(pair, "Value is required.");
-		Assert.isTrue(pair.getSecond().length == 2, "2 flags required.");
+		Assert.isTrue(pair.getSecond().length == 2, BOOLEAN_ARRAY_WRONG_SIZE);
 		final var day = pair.getFirst();
 		Assert.notNull(day, "Day is required.");
 		Assert.notNull(pair.getSecond(), "Flags required.");
 		final var dataGroupProcessed = pair.getSecond()[0];
 		final var cycleProcessed = pair.getSecond()[1];
-		System.out.println(dataGroupProcessed);
-		System.out.println(cycleProcessed);
-		final Collection<String> results = new ArrayList<>();
-		addDay(day, results);
-		return results.toArray(new String[results.size()]);
+		return Stream.concat(Stream.concat(day(day).stream(), dayGroup(day.dayGroup(), dataGroupProcessed)), cycle(day.dayGroup().cycle(), cycleProcessed))
+				.toArray(size -> new String[size]);
+
 	}
 
-	private void addDay(final Day<?> day, final Collection<String> results) {
-		results.add(day.getClass().getAnnotation(DiscriminatorValue.class).value());
+	private Stream<String> cycle(final Cycle cycle, final boolean processed) {
+		if (processed) {
+			return Stream.concat(Stream.of(id(cycle)), emptyColumns(3));
+		}
+		return Stream.of(id(cycle), quote(cycle.name()), String.valueOf(cycle.priority()), String.valueOf(cycle.isDeaultCycle()));
+	}
+
+	private Stream<String> dayGroup(final DayGroup dayGroup, final boolean processed) {
+		if (processed) {
+			return Stream.concat(Stream.of(id(dayGroup)), emptyColumns(2));
+
+		}
+		return Stream.of(id(dayGroup), quote(dayGroup.name()), String.valueOf(dayGroup.readOnly()));
+
+	}
+
+	private Collection<String> day(final Day<?> day) {
 		Assert.isTrue(valueConverter.containsKey(day.getClass()), String.format("ValueConveter missing for class %s", day.getClass().getSimpleName()));
-		results.add(valueConverter.get(day.getClass()).apply((AbstractDay<?>) day));
-		results.add(quote(string(day.description())));
-		results.add(id(day.dayGroup()));
+		return List.of(day.getClass().getAnnotation(DiscriminatorValue.class).value(), valueConverter.get(day.getClass()).apply((AbstractDay<?>) day),
+				quote(string(day.description())));
 	}
 
-	private String string(Optional<String> value) {
+	private String string(final Optional<String> value) {
 		return value.orElse("");
 	}
 
-	private String id(Object entity) {
+	private Stream<String> emptyColumns(final int count) {
+		return IntStream.range(0, count).mapToObj(i -> "");
+	}
+
+	private String id(final Object entity) {
 		final UUID uuid = UUID.fromString(IdUtil.getId(entity));
 		Assert.isTrue(uuid.getMostSignificantBits() == uuid.getLeastSignificantBits(), "Most and LeastSignificantBits should be the same.");
 		return "" + uuid.getMostSignificantBits();
@@ -75,6 +113,10 @@ class DayCsvConverterImpl implements Converter<Pair<Day<?>, boolean[]>, String[]
 			return OUOTE + result + OUOTE;
 		}
 		return result;
+	}
+
+	private String numberValue(final int value, int digits) {
+		return String.format("%" + digits + "d", value).replace(' ', '0');
 	}
 
 }
--- iot2.domain/src/test/java/de/mq/iot2/calendar/support/DayCsvConverterImplTest.java
+++ iot2.domain/src/test/java/de/mq/iot2/calendar/support/DayCsvConverterImplTest.java
@@ -1,37 +1,142 @@
 package de.mq.iot2.calendar.support;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.MonthDay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.util.Pair;
-import org.springframework.util.StringUtils;
 
 import de.mq.iot2.calendar.Cycle;
 import de.mq.iot2.calendar.Day;
 import de.mq.iot2.calendar.DayGroup;
+import de.mq.iot2.support.RandomTestUtil;
 
 class DayCsvConverterImplTest {
 
-	private static final long DAY_GROUP_ID = 1L;
+	private static final long CYCLE_ID = RandomTestUtil.randomLong();
+	private static final long DAY_GROUP_ID = RandomTestUtil.randomLong();
 	private static final String CSV_DELIMITER = ";";
 	private final Converter<Pair<Day<?>, boolean[]>, String[]> converter = new DayCsvConverterImpl(CSV_DELIMITER);
 
 	@Test
-	void convertFull() {
-		final Cycle cycle = new CycleImpl("Freizeit", 101, false);
-		final DayGroup dayGroup = new DayGroupImpl(cycle, DAY_GROUP_ID, "Feiertage", true);
-		final Day<?> day = new GaussDayImpl(dayGroup, -2, "Karfreitag");
+	void convertGaussDayFull() {
+		final Day<?> day = newGausDay(newDayGroup());
 
-		final String[] results = converter.convert(Pair.of(day, new boolean[] { true, true }));
+		final String[] results = converter.convert(Pair.of(day, new boolean[] { false, false }));
 
-		assertEquals(4, results.length);
+		assertEquals(10, results.length);
 		assertEquals(GaussDayImpl.ENTITY_NAME, results[0]);
 		assertEquals("-2", results[1]);
 		assertEquals(day.description().orElseThrow(), results[2]);
 		assertEquals("" + DAY_GROUP_ID, results[3]);
 
-		System.out.println(StringUtils.arrayToDelimitedString(results, CSV_DELIMITER));
+		assertEquals(day.dayGroup().name(), results[4]);
+		assertEquals("" + true, results[5]);
+		assertEquals("" + CYCLE_ID, results[6]);
+		assertEquals(day.dayGroup().cycle().name(), results[7]);
+		assertEquals("" + day.dayGroup().cycle().priority(), results[8]);
+		assertEquals("" + false, results[9]);
+
+	}
+
+	@Test
+	void convertGaussDayProcessed() {
+		final Day<?> day = newGausDay(newDayGroup());
+		final String[] results = converter.convert(Pair.of(day, new boolean[] { true, true }));
+
+		assertEquals(10, results.length);
+
+		assertEquals(10, results.length);
+		assertEquals(GaussDayImpl.ENTITY_NAME, results[0]);
+		assertEquals("-2", results[1]);
+		assertEquals(day.description().orElseThrow(), results[2]);
+		assertEquals("" + DAY_GROUP_ID, results[3]);
+
+		assertTrue(results[4].isEmpty());
+		assertTrue(results[5].isEmpty());
+		assertEquals("" + CYCLE_ID, results[6]);
+		assertTrue(results[7].isEmpty());
+		assertTrue(results[8].isEmpty());
+		assertTrue(results[9].isEmpty());
+	}
+
+	@Test
+	void convertWrongBooleanArray() {
+		assertEquals(DayCsvConverterImpl.BOOLEAN_ARRAY_WRONG_SIZE,
+				assertThrows(IllegalArgumentException.class, () -> converter.convert(Pair.of(newGausDay(newDayGroup()), new boolean[] { true }))).getMessage());
+	}
+
+	@Test
+	void convertDayOfMonth() {
+		final Day<?> day = new DayOfMonthImpl(newDayGroup(), MonthDay.of(5, 1), "Tag der Arbeit");
+
+		final String[] results = converter.convert(Pair.of(day, new boolean[] { true, true }));
+
+		assertEquals(10, results.length);
+		assertEquals(DayOfMonthImpl.ENTITY_NAME, results[0]);
+		assertEquals("01.05", results[1]);
+		assertEquals(day.description().orElseThrow(), results[2]);
+		assertEquals("" + DAY_GROUP_ID, results[3]);
+		assertTrue(results[4].isEmpty());
+		assertTrue(results[5].isEmpty());
+		assertEquals("" + CYCLE_ID, results[6]);
+		assertTrue(results[7].isEmpty());
+		assertTrue(results[8].isEmpty());
+		assertTrue(results[9].isEmpty());
+
+	}
+
+	@Test
+	void convertDayOfWeekDayImpl() {
+		final Day<?> day = new DayOfWeekDayImpl(newDayGroup(), DayOfWeek.SUNDAY);
+		final String[] results = converter.convert(Pair.of(day, new boolean[] { true, true }));
+
+		assertEquals(10, results.length);
+		assertEquals(DayOfWeekDayImpl.ENTITY_NAME, results[0]);
+		assertEquals("" + DayOfWeek.SUNDAY.getValue(), results[1]);
+		assertTrue(results[2].isEmpty());
+		assertEquals("" + DAY_GROUP_ID, results[3]);
+		assertTrue(results[4].isEmpty());
+		assertTrue(results[5].isEmpty());
+		assertEquals("" + CYCLE_ID, results[6]);
+		assertTrue(results[7].isEmpty());
+		assertTrue(results[8].isEmpty());
+		assertTrue(results[9].isEmpty());
+	}
+
+	@Test
+	void convertLocalDateDay() {
+		final Day<?> day = new LocalDateDayImp(newDayGroup(), LocalDate.of(1831, 6, 13));
+		final String[] results = converter.convert(Pair.of(day, new boolean[] { true, true }));
+
+		assertEquals(10, results.length);
+		assertEquals(LocalDateDayImp.ENTITY_NAME, results[0]);
+		assertEquals("13.06.1831", results[1]);
+		assertTrue(results[2].isEmpty());
+		assertEquals("" + DAY_GROUP_ID, results[3]);
+		assertTrue(results[4].isEmpty());
+		assertTrue(results[5].isEmpty());
+		assertEquals("" + CYCLE_ID, results[6]);
+		assertTrue(results[7].isEmpty());
+		assertTrue(results[8].isEmpty());
+		assertTrue(results[9].isEmpty());
+	}
+
+	private DayGroup newDayGroup() {
+		final Cycle cycle = new CycleImpl(CYCLE_ID, "Freizeit", 101, false);
+		final DayGroup dayGroup = new DayGroupImpl(cycle, DAY_GROUP_ID, "Feiertage", true);
+		return dayGroup;
+	}
+
+	private Day<?> newGausDay(final DayGroup dayGroup) {
+		final Day<?> day = new GaussDayImpl(dayGroup, -2, "Karfreitag");
+		return day;
 	}
 
 }
