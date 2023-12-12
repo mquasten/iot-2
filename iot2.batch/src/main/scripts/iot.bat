553821d2c46ad228b299e49d298129b38e290bee
Nutzer <manfred.quasten@t-online.de>
2023-12-12 10:31:21 +0100

    add protocol.csv

--- iot2.batch/src/main/scripts/iot.bat
+++ iot2.batch/src/main/scripts/iot.bat
@@ -9,8 +9,14 @@ java -jar C:\iot/lib/iot2Batch.jar -c export-calendar C:\iot/backup/calendar.csv
 echo start export-configuration at: %DATE% %TIME%  >> C:\iot/log/iot.log 2>>&1
 java -jar C:\iot/lib/iot2Batch.jar -c export-configuration C:\iot/backup/configuration.csv >> C:\iot/log/iot.log 2>>&1
 
-echo start cleanup at: %DATE% %TIME%  >> C:\iot/log/iot.log 2>>&1
-java -jar C:\iot/lib/iot2Batch.jar -c cleanup >> C:\iot/log/iot.log 2>>&1
+echo start export-protocol at: %DATE% %TIME%  >> C:\iot/log/iot.log 2>>&1
+java -jar C:\iot/lib/iot2Batch.jar -c export-protocol C:\iot/backup/protocol.csv >> C:\iot/log/iot.log 2>>&1
+
+echo start cleanup-calendar at: %DATE% %TIME%  >> C:\iot/log/iot.log 2>>&1
+java -jar C:\iot/lib/iot2Batch.jar -c cleanup-calendar >> C:\iot/log/iot.log 2>>&1
+
+echo start cleanup-protocol at: %DATE% %TIME%  >> C:\iot/log/iot.log 2>>&1
+java -jar C:\iot/lib/iot2Batch.jar -c cleanup-protocol >> C:\iot/log/iot.log 2>>&1
 
 echo start db backup at: %DATE% %TIME%  >> C:\iot/log/iot.log 2>>&1
 java -cp "lib/h2-2.1.214.jar"  org.h2.tools.Script -url jdbc:h2:tcp://localhost/iot -user sa -password sa -script backup/backup.sql >> C:\iot/log/iot.log 2>>&1
