eaff27bd225709e7dff2c87724c38db2cd447bd9
Nutzer <manfred.quasten@t-online.de>
2024-01-07 18:58:21 +0100

    cosmetics

--- iot2.batch/docker/script/iot.bat
+++ iot2.batch/docker/script/iot.bat
@@ -1,5 +1,3 @@
-
-
 echo start end-of-day at: %DATE% %TIME%  >> C:\iot/log/iot.log 2>>&1
 docker  run  -v C:/iot/h2:/db  iot2batch  -c end-of-day >> C:\iot/log/iot.log 2>>&1
 
@@ -21,4 +19,8 @@ docker  run  -v C:/iot/h2:/db  iot2batch  -c cleanup-protocol >> C:\iot/log/iot.
 echo start db backup at: %DATE% %TIME%  >> C:\iot/log/iot.log 2>>&1
 docker  run  -v C:/iot/h2:/db  -v C:/iot/backup:/backup iot2batch ! java -cp //h2/h2-2.1.214.jar  org.h2.tools.Script -url jdbc:h2:tcp://localhost/iot -user sa -password sa -script //backup/backup.sql >> C:\iot/log/iot.log 2>>&1
 
+echo start docker aufräumen: %DATE% %TIME%  >> C:\iot/log/iot.log 2>>&1
+docker system prune -f
+
 REM shutdown /s /f /t 00 >>  C:\iot/log/iot.log 2>>&1
+
