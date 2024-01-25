49dc84bc25489e001f0bc9135fa9060f4847e130
Nutzer <manfred.quasten@t-online.de>
2024-01-25 20:34:52 +0100

    spring.output.ansi.enabled=NEVER logs nicht farbig ping ... -4

--- iot2.web/src/main/scripts/endOfDayBatchDocker.bat
+++ iot2.web/src/main/scripts/endOfDayBatchDocker.bat
@@ -1,16 +1,17 @@
 docker rm -f iot2  >NUL 2>&1
 
-docker run  -d --name iot2 -p 8080:8080   -p 1522:1521  -v C:\iot/oracle:/opt/oracle/oradata  -v C:\iot/backup:/backup    iot2
+REM docker run  -d --name iot2 -p 8080:8080   -p 1522:1521  -v C:\iot/oracle:/opt/oracle/oradata  -v C:\iot/backup:/backup    iot2
 
+docker run  -d --name iot2 -p 8080:8080  -p 9092:9092 -p 8082:8082 -v C:\iot/h2:/db -v C:\iot/backup:/backup   iot2
+
+REM timeout 30 > NUL
 docker logs  iot2  >> C:\iot/log/iot.log 2>>&1
-timeout 30 > NUL
 docker exec -it iot2 endOfDay   >> C:\iot/log/iot.log 2>>&1
+docker exec -it iot2 backup   >> C:\iot/log/iot.log 2>>&1
+echo.  >> C:\iot/log/iot.log 2>>&1
 docker stop iot2  >NUL 2>&1
 docker rm -f iot2  >NUL 2>&1
 docker system prune -f  >NUL 2>&1
 
 
 
-REM docker run  -v C:/iot/h2:/db  -v C:/iot/backup:/backup iot2 ! endOfDay.sh >> C:\iot/log/iot.log 2>>&1
-REM echo start docker aufräumen: %DATE% %TIME%  >> C:\iot/log/iot.log 2>>&1
-REM  docker system prune -f
