46171ae657084522cc49af02a8b998668fe5162e
Manfred Quasten <manfred.quasten@binaris-informatik.de>
2024-02-06 11:29:05 +0100

    dockerfiles h2 überarbeitet

---
+++ iot2.batch/docker/h2/dbready.sh
@@ -0,0 +1,1 @@
+curl --http0.9  --fail --output /dev/null  http://localhost:9092  > /dev/null 2>&1 ;
--- iot2.batch/docker/scripts/autoload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for script in $HOME/startup/*.sh
-do
-   if [ -x $script ]; then
-      echo "execute $script";
-      $script;
-   fi;
-done
