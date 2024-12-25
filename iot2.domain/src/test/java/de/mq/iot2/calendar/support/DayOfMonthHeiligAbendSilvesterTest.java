a9f57e1b0a2e0608e97f5b46b323a7871794cb92
Nutzer <manfred.quasten@t-online.de>
2024-12-25 17:14:24 +0100

    Inserts Heiligabend und Silvester, new DayGroup

---
+++ iot2.domain/src/test/java/de/mq/iot2/calendar/support/DayOfMonthHeiligAbendSilvesterTest.java
@@ -0,0 +1,28 @@
+package de.mq.iot2.calendar.support;
+
+import java.time.MonthDay;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import de.mq.iot2.calendar.Cycle;
+import de.mq.iot2.calendar.Day;
+import de.mq.iot2.calendar.DayGroup;
+import de.mq.iot2.support.IdUtil;
+
+class DayOfMonthHeiligAbendSilvesterTest {
+	
+	@Test
+	void createdays() {
+		final Cycle cycle = new CycleImpl(1L, "Freizeit" ,102, false);
+		final DayGroup dayGroup = new DayGroupImpl(cycle, 6, "Urlaub Tarifvertrag");
+		final  Collection<Day<?>>  days = List.of(new DayOfMonthImpl(dayGroup, MonthDay.of(12, 24), "Heiligabend" ), new  DayOfMonthImpl(dayGroup, MonthDay.of(12, 31), "Silvester" ));
+		
+        System.out.println( String.format("INSERT INTO DAY_GROUP(ID,NAME,READ_ONLY,CYCLE_ID) VALUES('%s','%s',1,'%s');",IdUtil.getId(dayGroup), dayGroup.name(), IdUtil.getId(dayGroup.cycle()) )) ;
+		
+        days.forEach(day ->  System.out.println(String.format("INSERT INTO SPECIAL_DAY(ID, DAY_TYPE, DAY_VALUE, DESCRIPTION, DAY_GROUP_ID) VALUES('%s', 'DayOfMonth', %s );", IdUtil.getId(day), ReflectionTestUtils.getField(day, "value"), day.description(), IdUtil.getId(day.dayGroup()) )));
+	    
+	}
+}
