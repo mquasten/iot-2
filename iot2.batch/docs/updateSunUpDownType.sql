d61a33ea96861b933ecaba390d25852f93aee08c
Nutzer <manfred.quasten@t-online.de>
2025-03-12 18:24:19 +0100

    updateScript sun up and down time

---
+++ iot2.batch/docs/updateSunUpDownType.sql
@@ -0,0 +1,5 @@
+update  parameter set parameter_value = 'Civil' where parameter_key = 'SunUpDownType'
+
+-- Mathematical, Civil, Nautical, Astronomical;
+
+select * from parameter where parameter_key = 'SunUpDownType'
